4c78f34d32075ac924b22e0e24cd2a673f381132
Lappicy <lappicy@gmail.com>
2023-07-08 11:59:51 -0300

    Add files via upload

---
+++ R/StreamflowEnergy.R
@@ -0,0 +1,15 @@
+#' @title Time series of streamflow
+#'
+#' @description
+#' ime series for streamflow energy for 4 subsystems in Brazil
+#'
+#' @format A five column data.frame with
+#' \describe{
+#' \item{Date is the Date for the observed values}
+#' \item{Subsystem_N is the Northern Brazilian Subsystem}
+#' \item{Subsystem_NE is the North East Brazilian Subsystem}
+#' \item{Subsystem_S is the Southern Brazilian Subsystem}
+#' \item{Subsystem_SE is the South East / Mid West Brazilian Subsystem}
+#' }
+#'
+"ClimaticInfo"
---
+++ man/ClimaticInfo.R
@@ -0,0 +1,14 @@
+#' @title Climatic indicators for forecasting
+#'
+#' @description
+#' Three climatic indicators (U1, SST2 and NINO3) time series from 1939 to 2021.
+#'
+#' @format A four column data.frame with
+#' \describe{
+#' \item{Date is the Date for the observed values}
+#' \item{U1 gives the Zonal Winds values}
+#' \item{SST2 is the sea surface temperature in part of the Atlancic}
+#' \item{NINO3 is the sea surface temperature at the Nino 3.4 region in the Pacific}
+#' }
+#'
+"ClimaticInfo"
