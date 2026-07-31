accf51ec66b93d2da039d696600c41485b4a81af
Lappicy <lappicy@gmail.com>
2026-07-31 10:09:42 -0300

    Delete run_app.R

--- run_app.R
+++
@@ -1,7 +0,0 @@
-runISPaL <- function(launch.browser = interactive(), ...) {
-  app_dir <- normalizePath(
-    if (file.exists("app.R")) "." else "ISPaL_Shiny",
-    mustWork = TRUE
-  )
-  shiny::runApp(appDir = app_dir, launch.browser = launch.browser, ...)
-}
