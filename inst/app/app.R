17c9b8919745244213c6737bd38bb986463baa5e
Lappicy <lappicy@gmail.com>
2026-07-31 10:10:46 -0300

    Delete inst directory

--- inst/app/app.R
+++
@@ -1,6 +0,0 @@
-app_dir <- system.file("app", package = "ISPaLShiny")
-if (!nzchar(app_dir) || !dir.exists(app_dir)) {
-  stop("Não foi possível localizar os arquivos do painel ISPaL.", call. = FALSE)
-}
-
-ISPaLShiny:::build_ispal_app(app_dir)
